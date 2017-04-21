bef1de9a51ac6453b1f3ac5251c35f898fac65e0
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-22 00:33:47 +0100

    .

--- main.cpp
+++ main.cpp
@@ -6,9 +6,29 @@ using namespace std;
 const int rows = 10;
 const int elements = 10;
 
+int maxS = 5;
+
 int matrix[rows][elements];
 
+void clear(){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < elements; j++){
+            matrix[i][j] = 0;
+        }
+    }
+}
+
+void show(){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < elements; j++){
+            cout << matrix[i][j] << " ";
+        }
+    }
+}
+
 int main() {
+    clear();
+    show();
     system("pause");
     return 0;
 
